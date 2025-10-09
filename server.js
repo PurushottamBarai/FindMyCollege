c9509d3f8f20f71a7ce6e4234433eb749738d3cd
PurushottamBarai <purushottambarai01@gmail.com>
2025-10-09 22:45:48 +0530

    Add dependencies for Railway deploy

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
